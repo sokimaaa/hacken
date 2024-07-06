fa8db044785d3dad38fb5c3fb1ead448c186e263
rdenysov <roman.denysov@intellias.com>
2024-07-06 12:15:04 +0300

    add workaround to fix filter exception

--- src/main/java/com/sokima/hacken/infrastructure/driven/starter/EthConnectAdapter.java
+++ src/main/java/com/sokima/hacken/infrastructure/driven/starter/EthConnectAdapter.java
@@ -3,7 +3,9 @@ package com.sokima.hacken.infrastructure.driven.starter;
 import com.sokima.hacken.domain.NodeTransaction;
 import com.sokima.hacken.domain.port.out.ConnectEvmNodeOutPort;
 import com.sokima.hacken.domain.port.out.SaveTransactionOutPort;
+import com.sokima.hacken.infrastructure.driven.starter.timer.Timer;
 import com.sokima.hacken.usecase.EthNodePropertiesUseCase;
+import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.slf4j.Logger;
@@ -11,18 +13,25 @@ import org.slf4j.LoggerFactory;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 
+import java.util.Objects;
+
 @ApplicationScoped
 public class EthConnectAdapter implements ConnectEvmNodeOutPort {
 
     private static final Logger log = LoggerFactory.getLogger(EthConnectAdapter.class);
 
     private static final long RETRY_NUMBER = 10L;
+    private static final long REFRESH_RATE_MINUTES = 1L;
 
     private final EthNodePropertiesUseCase ethProperties;
 
     private final SaveTransactionOutPort saveTransactionOutPort;
 
-    private final Web3j web3j;
+    private final HttpService httpService;
+
+    private Web3j web3j;
+
+    private final Timer timer;
 
     @Inject
     public EthConnectAdapter(
@@ -31,11 +40,22 @@ public class EthConnectAdapter implements ConnectEvmNodeOutPort {
     ) {
         this.ethProperties = ethProperties;
         this.saveTransactionOutPort = saveTransactionOutPort;
-        this.web3j = Web3j.build(new HttpService(ethProperties.nodeUrl()));
+        this.httpService = new HttpService(ethProperties.nodeUrl());
+        this.timer = Timer.start();
+    }
+
+    @Scheduled(every = "60s")
+    void checkConnectionState() {
+        if (Objects.nonNull(timer) && timer.isTimePassed(REFRESH_RATE_MINUTES)) {
+            log.info("{} minutes passed without updates, initiated refreshing of {}.", REFRESH_RATE_MINUTES, getClass().getSimpleName());
+            reconnect();
+        }
     }
 
     @Override
     public void connect() {
+        timer.updateTime();
+        web3j = Web3j.build(httpService);
         web3j.transactionFlowable()
                 .map(
                         tx -> new NodeTransaction(
@@ -51,6 +71,7 @@ public class EthConnectAdapter implements ConnectEvmNodeOutPort {
                         )
                 )
                 .doOnNext(tx -> log.info("Inbound eth tx: {}", tx))
+                .doOnNext(tx -> timer.updateTime())
                 .retry(RETRY_NUMBER)
                 .subscribe(saveTransactionOutPort::saveTx, ex -> {
                     log.warn("Error while saving eth tx: {}", ex.getMessage());
@@ -59,6 +80,7 @@ public class EthConnectAdapter implements ConnectEvmNodeOutPort {
     }
 
     private void reconnect() {
+        web3j.shutdown();
         connect();
     }
 }
---
+++ src/main/java/com/sokima/hacken/infrastructure/driven/starter/timer/Timer.java
@@ -0,0 +1,43 @@
+package com.sokima.hacken.infrastructure.driven.starter.timer;
+
+import java.time.Duration;
+import java.time.Instant;
+
+/**
+ * Workaround to fix:
+ * WARN  [org.web.pro.cor.fil.Filter] (pool-9-thread-1) The filter has not been found. Filter id: 14621507953634074608218513988515298149
+ * ERROR [org.web.pro.cor.fil.Filter] (pool-9-thread-1) Error sending request: org.web3j.protocol.core.filters.FilterException: Error sending request
+ *         at org.web3j.protocol.core.filters.Filter.throwException(Filter.java:194)
+ *         at org.web3j.protocol.core.filters.Filter.run(Filter.java:104)
+ *         at org.web3j.protocol.core.filters.Filter.reinstallFilter(Filter.java:155)
+ *         at org.web3j.protocol.core.filters.Filter.pollFilter(Filter.java:137)
+ *         at org.web3j.protocol.core.filters.Filter.lambda$run$0(Filter.java:92)
+ *         at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:539)
+ *         at java.base/java.util.concurrent.FutureTask.runAndReset(FutureTask.java:305)
+ *         at java.base/java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.run(ScheduledThreadPoolExecutor.java:305)
+ *         at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1136)
+ *         at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:635)
+ *         at java.base/java.lang.Thread.run(Thread.java:840)
+ * Caused by: java.io.InterruptedIOException: interrupted
+ */
+public final class Timer {
+
+    private Instant lastUpdateTime;
+
+    private Timer(final Instant lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public static Timer start() {
+        return new Timer(Instant.now());
+    }
+
+    public void updateTime() {
+        this.lastUpdateTime = Instant.now();
+    }
+
+    public boolean isTimePassed(final long minutes) {
+        final var timePassedInMinutes = Duration.between(lastUpdateTime, Instant.now()).toMinutes();
+        return timePassedInMinutes > minutes;
+    }
+}
